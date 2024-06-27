623bd9cffbe6253cb8e754a0f7a42eb929af9b9a
Olha Bezklepchenko <krolichek.11@gmail.com>
2024-06-27 19:52:54 +0200

    add changes  task-4

--- js/task-4.js
+++ js/task-4.js
@@ -9,23 +9,22 @@ function handleSubmit(event) {
   event.preventDefault();
 
   // Отримуємо форму з об'єкта події і введенні в поля форми значенняб видаляючи пробіли методом .trim()
-  const form = event.target;
+  const form = event.currentTarget;
   const email = form.elements.email.value.trim();
   const password = form.elements.password.value.trim();
 
   // Створюємо об'єкт для збереження введених значень
-  const objFormSubmit = {};
+  const info = {
+    email,
+    password,
+  };
 
   // Якщо є незаповнені поля, виводимо попередження
   if (email === "" || password === "") {
     alert("All form fields must be filled in");
   } else {
-    // Якщо всі поля заповнені, додаємо значення до об'єкта
-    objFormSubmit.email = email;
-    objFormSubmit.password = password;
-
-    // Виводимо об'єкт із введеними даними в консоль і очищуємо значення полів форми методом reset.
-    console.log(objFormSubmit);
+    // Якщо всі поля заповнені, виводимо об'єкт із введеними даними в консоль і очищуємо значення полів форми методом .reset().
+    console.log(info);
     form.reset();
   }
 }
