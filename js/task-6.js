bcb5e5f4fbba315cb22dc86b1b9abd22489cc8f1
Olha Bezklepchenko <krolichek.11@gmail.com>
2024-06-28 19:15:37 +0200

    change task-6

--- js/task-6.js
+++ js/task-6.js
@@ -1,30 +1,20 @@
-// Напиши скрипт створення й очищення колекції елементів з наступним функціоналом.
-// Є input, у який користувач вводить бажану кількість елементів. Після натискання на кнопку Create має рендеритися (додаватися в DOM) колекція з відповідною кількістю елементів і очищатися значення в інпуті. При повторному натисканні на кнопку Create поверх старої колекції має рендеритись нова. Після натискання на кнопку Destroy колекція елементів має очищатися.
-// Після натискання користувачем на кнопку Create треба провалідувати значення в input, воно має бути в межах від 1 до 100 включно. Тільки якщо воно задоволяє умову, мають додаватися нові <div> елементи в DOM.
-
-// Для рендеру елементів на сторінці створи функцію createBoxes(amount), яка приймає один параметр — число, що зберігає кількість елементів для рендеру.
-
-// Функція має створювати стільки <div> елементів, скільки вказано в параметрі amount і додавати їх у DOM дочірніми елементами для div#boxes.
-
-// Розміри першого <div> елемента мають бути 30px на 30px.
-// Кожен наступний елемент повинен бути ширшим і вищим від попереднього на 10px.
-// Усі елементи повинні мати випадковий колір фону. Використовуй готову функцію getRandomHexColor() для отримання випадкового кольору.
-// Для очищення колекції після натискання на кнопку Destroy створи функцію destroyBoxes(), яка очищає вміст div#boxes, у такий спосіб видаляючи всі створені елементи.
-
+//функція рандомного вибору кольору
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
-
+// отримуємо доступ до необхідних елементів
 const inputNumber = document.querySelector("#controls input");
 const buttonCreate = document.querySelector("button[data-create]");
 const buttonDestroy = document.querySelector("button[data-destroy]");
 const boxes = document.querySelector("#boxes");
 
+// додаємо слухача на кнопки
 buttonCreate.addEventListener("click", handleCreate);
 buttonDestroy.addEventListener("click", destroyBoxes);
 
+// функція, що валідує значення,введене в інпут і відносно значення викликає функцію створення нових елементів, або алерт-повідомлення.
 function handleCreate() {
   const amount = Number(inputNumber.value);
 
@@ -36,20 +26,31 @@ function handleCreate() {
   }
 }
 
+// функція створення елементів
 function createBoxes(amount) {
   boxes.innerHTML = "";
 
   let boxSize = 30;
+  // створюємо масив який будемо наповнювати створеними елементами
+  const boxElements = [];
 
+  // цикл, який наповнює масив елементів і збільшує розміри елемента з кожною ітерацією
   for (let i = 0; i < amount; i++) {
     const box = document.createElement("div");
+
     box.style.width = `${boxSize}px`;
     box.style.height = `${boxSize}px`;
     box.style.backgroundColor = getRandomHexColor();
-    boxes.appendChild(box);
+
+    boxElements.push(box);
+
     boxSize += 10;
   }
+  // додаємо елементи в DOM одночасно за допомогою методу .append() і оператору spread
+  boxes.append(...boxElements);
 }
+
+// функція знищення елементів
 
 function destroyBoxes() {
   boxes.innerHTML = "";
